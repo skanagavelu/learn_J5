a8d0f1d4ac4a3581fd8ed53c6350bfad46bf5ec1
I311774 <I311774@INLD50877170A.apj.global.corp.sap>
2016-03-28 14:29:13 +0530

    .

--- src/generics/Casting.java
+++ src/generics/Casting.java
@@ -14,15 +14,39 @@ public class Casting {
 		//http://stackoverflow.com/questions/12369957/dealing-with-an-arraystoreexception
 		Object[] objArray = new String[10]; //This should be avoided.
 		objArray[0] = new String();
-		objArray[0] = new Object();//RE: java.lang.ArrayStoreException: java.lang.Object at Casting.main(Casting.java:13)
+//		objArray[0] = new Object();//RE: java.lang.ArrayStoreException: java.lang.Object at Casting.main(Casting.java:13)
 		
 		
 		
 //		http://stackoverflow.com/questions/18666710/why-are-arrays-covariant-but-generics-are-invariant
 		
+		List<SubClass> subClassList = new ArrayList<SubClass>();
+//		subClassList.add(new SuperClass()); CE: The method add(SubClass) in the type List<SubClass> is not applicable for the arguments (SuperClass)
+
+		List<SuperClass> superClassList = new ArrayList<SuperClass>();
+		superClassList.add(new SuperClass()); 		
+		superClassList.add(new SubClass()); 
+//		superClassList = (List<SuperClass>) subClassList; CE: Cannot cast from List<SubClass> to List<SuperClass>
+		
+		
+		
+		
 //		List<SuperClass> superList = new ArrayList<SubClass>(); //CE: Type mismatch: cannot convert from ArrayList<SubClass> to List<SuperClass>
+		List subClassDubList = subClassList;
+		List<SuperClass> superClassDubList = subClassDubList;
+		superClassDubList.add(new SuperClass()); //Corrupting the list with non generic.
+				
+		
+		List<? extends SuperClass> list_4__A_AND_SubClass_A = new ArrayList<SubClass>();
+		List<SuperClass> list_A = (ArrayList<SuperClass>)list_4__A_AND_SubClass_A; //Warning: Type safety: Unchecked cast from List<capture#1-of ? extends SuperClass> to ArrayList<SuperClass>
+		list_A.add(new SuperClass()); // No exception here, Why? Because you have ignored the warning.
+		
+		
+		
 		List<?> superList = new ArrayList<SubClass>();   // Using this we can only read; we cannot write
 //		superList.add(new SubClass());     //CE: The method add(capture#1-of ?) in the type List<capture#1-of ?> is not applicable for the arguments (SubClass)
+		
+		
 		
 		
 		//http://stackoverflow.com/questions/2745265/is-listdog-a-subclass-of-listanimal-why-arent-javas-generics-implicitly-p/2745301#2745301
@@ -38,9 +62,29 @@ public class Casting {
 		 *superExtendsList.add(new SubClass()); 
 		 */
 		
-		    
-
+		//http://stackoverflow.com/questions/2745265/is-listdog-a-subclass-of-listanimal-why-arent-javas-generics-implicitly-p/2745301#2745301
+		List<? super SuperClass> superSuperList = new ArrayList<SuperClass>();
+		superSuperList.add(new SubClass());
+		superSuperList.add(new SubSubClass());
+		superSuperList.add(new SuperClass());
+//		superSuperList.add(new Object()); CE: The method add(capture#4-of ? super SuperClass) in the type List<capture#4-of ? super SuperClass> is not applicable for the arguments (Object)
+//		superSuperList.add(new String()); CE: The method add(capture#3-of ? super SuperClass) in the type List<capture#3-of ? super SuperClass> is not applicable for the arguments (String)
+		superSuperList.get(0);
+		superSuperList.get(1);
+		superSuperList.get(2);
+		
+		List noGenericList = new ArrayList();
+		noGenericList.add(new SubClass());
+		noGenericList.add(new SubSubClass());
+		noGenericList.add(new SuperClass());
+		noGenericList.add(new Object());
+		noGenericList.add(new String());
+		
+		
+		
 		System.out.println(Arrays.toString(arraySuper));
+		System.out.println(superSuperList);
+		System.out.println(noGenericList);
 	}
 }
 
@@ -51,3 +95,11 @@ class SuperClass {
 class SubClass extends SuperClass {
 
 }
+
+class SubSubClass extends SubClass {
+
+}
+
+class A {}
+class B extends A {}
+
